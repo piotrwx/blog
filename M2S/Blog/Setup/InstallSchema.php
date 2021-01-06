e0c6e3b2bc27b26aef20d38828d575fce04002ff
piotrwx <piotr.wlosekx@gmail.com>
2021-01-06 13:05:40 +0100

    Create database schema module M2S Blog 0.0.15

---
+++ M2S/Blog/Setup/InstallSchema.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace M2S\Blog\Setup;
+
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+class InstallSchema implements InstallSchemaInterface
+{
+
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+            $table = $setup->getConnection()->newTable(
+                $setup->getTable('m2s_blog')
+            )->addColumn(
+                'id',
+                Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'nullable' => false, 'primary' => true],
+                'Post ID'
+            )->addColumn(
+                'title',
+                Table::TYPE_TEXT,
+                null,
+                ['nullable' => false],
+                'Post title'
+            )->addColumn(
+                'created_at',
+                Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => Table::TIMESTAMP_INIT],
+                'Post created time'
+            )->addIndex(
+                $setup->getIdxName('m2s_blog', ['id']),
+                ['name']
+            )->setComment('M2S Blog');
+
+        $setup->endSetup();
+    }
+}
